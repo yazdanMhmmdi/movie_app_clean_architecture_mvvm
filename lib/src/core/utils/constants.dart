145c9be92397ac3362b5c489ace5a60d89d6b131
Yazdan Mohammadi <yazdanmohammadi00@gmail.com>
2022-01-01 16:44:16 +0330

    add some constants

---
+++ lib/src/core/utils/constants.dart
@@ -0,0 +1,8 @@
+//  API URL
+const String? kBaseUrl = "https://api.themoviedb.org/3";
+//  API Key
+const String? kApiKey = '03df62f0b7d7601bd76409972e41f171';
+//  Language
+const String? kLanguage = 'fa-IR';
+//  Sort Option
+const String? kSortBy = 'popularity.desc';
