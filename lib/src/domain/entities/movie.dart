7b966e9f8115287c7c06618fe3623aaa531827bc
Yazdan Mohammadi <yazdanmohammadi00@gmail.com>
2022-01-01 15:47:27 +0330

    add entity

---
+++ lib/src/domain/entities/movie.dart
@@ -0,0 +1,54 @@
+import 'package:equatable/equatable.dart';
+
+class Movie extends Equatable {
+  bool? adult;
+  String? backdropPath;
+  List<int>? genreIds;
+  double? id;
+  String? originalLanguage;
+  String? originalTitle;
+  String? overview;
+  double? popularity;
+  String? posterPath;
+  String? releaseDate;
+  String? title;
+  bool? video;
+  double? voteAverage;
+  int? voteCount;
+  Movie({
+    this.adult,
+    this.backdropPath,
+    this.genreIds,
+    this.id,
+    this.originalLanguage,
+    this.originalTitle,
+    this.overview,
+    this.popularity,
+    this.posterPath,
+    this.releaseDate,
+    this.title,
+    this.video,
+    this.voteAverage,
+    this.voteCount,
+  });
+  @override
+  List<Object?> get props => [
+        adult,
+        backdropPath,
+        genreIds,
+        id,
+        originalLanguage,
+        originalTitle,
+        overview,
+        popularity,
+        posterPath,
+        releaseDate,
+        title,
+        video,
+        voteAverage,
+        voteCount,
+      ];
+
+  @override
+  bool? get stringify => true;
+}
